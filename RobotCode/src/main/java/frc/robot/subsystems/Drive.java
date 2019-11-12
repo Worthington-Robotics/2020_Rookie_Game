57113c4c06d69b93efe5ee6115a0e1fb914cf75a
PhillipWhiter <34491601+PhillipWhiter@users.noreply.github.com>
2019-11-12 10:28:29 -0500

    Next commit

---
+++ RobotCode/src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+
+public class Drive extends Subsystem {
+    Drive mDrive = new Drive();
+
+    public Drive GetInstance()
+    {
+        return mDrive;
+    } 
+    @Override
+    public void outputTelemetry() {
+        
+    }
+    @Override
+    public void reset() {
+        
+    }
+
+}
