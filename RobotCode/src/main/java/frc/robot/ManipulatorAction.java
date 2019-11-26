d16b812dcd5708b031170abd1ba7ab91cd7e214c
rt0836 <rbtate19@gmail.com>
2019-11-26 17:01:44 -0500

    Manipulator + Action

---
+++ RobotCode/src/main/java/frc/robot/ManipulatorAction.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+import frc.lib.statemachine.Action;
+import frc.robot.subsystems.Manipulator;
+
+public class ManipulatorAction extends Action {
+
+
+    /**
+     * code to run on action start
+     */
+    @Override
+    public void onStart() {
+        Manipulator.getInstance().something(-1);
+    }
+
+    /**
+     * code to run while action loops
+     * <p>approx every 20 miliseconds
+     */
+    @Override
+    public void onLoop() {
+
+    }
+
+    /**
+     * method that tells the state machine the action is finished earlier than the scheduler
+     *
+     * @return true when action is ready to self terminate
+     */
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+    /**
+     * code to run when the action has ben called by the state machine to stop
+     */
+    @Override
+    public void onStop() {
+        Manipulator.getInstance().something(0);
+    }
+}
---
+++ RobotCode/src/main/java/frc/robot/subsystems/Manipulator.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import frc.lib.loops.ILooper;
+import frc.robot.Constants;
+
+public class Manipulator extends Subsystem {
+
+    private PeriodicIO periodicIO;
+    private Spark motor;
+
+    public Manipulator() {
+        motor = new Spark(Constants.MOTOR_ID);
+        periodicIO = new PeriodicIO();
+    }
+
+    @Override
+    public void writePeriodicOutputs() {
+        motor.set(periodicIO.motor_demand);
+    }
+
+
+    public void something(double w){
+        periodicIO.motor_demand = w;
+    }
+    public static Manipulator getInstance() {
+        return new Manipulator();
+    }
+    /**
+     * Outputs all logging information to the SmartDashboard
+     */
+
+    public void registerEnabledLoops(ILooper enabledLooper) {
+
+    }
+
+    @Override
+    public void outputTelemetry() {
+
+    }
+
+    /**
+     * Called to reset and configure the subsystem
+     */
+    @Override
+    public void reset() {
+
+    }
+
+    public static class PeriodicIO {
+        public double motor_demand = 0;
+    }
+}
